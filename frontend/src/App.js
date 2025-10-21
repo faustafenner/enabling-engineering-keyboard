cbb731f02bbe07e6384a1eeff9a6dcabc4c07f4e
faustafenner <fennerfausta@gmail.com>
2025-10-21 12:15:17 -0400

    lights off function added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Input from './Input';
 import Display from './Display';
 import Success from './Success';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:5050/lights_off', { method: 'POST' })
+      .then(() => console.log('Requested lights off on app load'))
+      .catch(err => console.error('Failed to request lights off:', err));
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
